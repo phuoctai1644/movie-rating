94a3dbb3bff32874ef6a69cd6558d8a773be608c
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-07-17 23:50:48 +0700

    feat: implement header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,19 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Genre } from '../../models';
+import { GenreComponent } from '../genre/genre.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgFor, GenreComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  genres: Genre[] = [
+    { id: 1, name: 'Drama' },
+    { id: 2, name: 'Action' },
+    { id: 3, name: 'Thriller' },
+  ]
 }
